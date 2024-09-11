39cd31ab6667efaeb2b6aaee53cf8084e8c4fa0b
Muhammad Sulaiman <almani.sulaiman@yahoo.com>
2024-09-11 12:33:14 -0400

    Fixed iperf container

--- configs/iperf/config/start_ping.sh
+++
@@ -1,4 +0,0 @@
-# Ping 10.41.0.2 to 10.41.0.100 using the following command:
-# echo host1 host2 host3 ... | xargs -n1 -P0 ping -c 4
-
-for ip in $(seq 2 6); do echo "10.41.0.$ip"; done | xargs -n1 -P0 ping
