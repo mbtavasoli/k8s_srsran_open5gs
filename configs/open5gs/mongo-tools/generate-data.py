93816371d7389ca1ed1f7a1eca312c4d6cdb4e98
mbtavasoli <78649156+mbtavasoli@users.noreply.github.com>
2025-10-18 21:44:45 +0200

    Update slice values: sst, sd

--- configs/open5gs/mongo-tools/generate-data.py
+++ configs/open5gs/mongo-tools/generate-data.py
@@ -1,29 +1,38 @@
 from open5gs import Open5GS
 from collections import defaultdict
 from logger import log
-import json
+from ruamel.yaml import YAML
+import argparse
 
-DEFAULT_UPLINK_SPEED = {"value": 1, "unit": Open5GS.Unit.Gbps}
-DEFAULT_DOWNLINK_SPEED = {"value": 1, "unit": Open5GS.Unit.Gbps}
-DEFAULT_QOS_INDEX = 9
-DEFAULT_ARP_VALUES = {
-    "priority_level": 8,
-    "pre_emption_capability": Open5GS.Status.DISABLED,
-    "pre_emption_vulnerability": Open5GS.Status.DISABLED,
+yaml = YAML()
+
+######################### CONFIGURATION ###################################
+DEFAULT_CONFIG = {
+    "NUM_SLICES": 2,  # Number of network slices to create
+    "NUM_COTS_SUBSCRIBERS": 0,  # Number of COTS subscribers
+    "NUM_SAMPLE_SUBSCRIBERS": 9,  # Number of simulated subscribers
+    "NUM_AUTO_GENERATED_SUBSCRIBERS": 0,  # Number of auto-generated subscribers (for MSD deployment only)
+    "DEFAULT_UPLINK_SPEED": {"value": 1, "unit": Open5GS.Unit.Gbps},
+    "DEFAULT_DOWNLINK_SPEED": {"value": 1, "unit": Open5GS.Unit.Gbps},
+    "DEFAULT_QOS_INDEX": 9,
+    "DEFAULT_ARP_VALUES": {
+        "priority_level": 8,
+        "pre_emption_capability": Open5GS.Status.DISABLED,
+        "pre_emption_vulnerability": Open5GS.Status.DISABLED,
+    },
+    "DEFAULT_KEY": "fec86ba6eb707ed08905757b1bb44b8f",
+    "DEFAULT_OPC": "C42449363BBAD02B66D16BC975D77CC1",
+    "DATA_DIR": "data",
+    "SLICE_FILE_PATH": "data/slices.yaml",
+    "SUBSCRIBER_FILE_PATH": "data/subscribers.yaml"
 }
 
-
-DEFAULT_KEY = "465B5CE8B199B49FAA5F0A2EE238A6BC"
-DEFAULT_OPC = "E8ED289DEBA952E4283B54E88E6183CA"
-DATA_DIR = "../data"
-
-SLICE_FILE_PATH = f"{DATA_DIR}/slices.json"
-SUBSCRIBER_FILE_PATH = f"{DATA_DIR}/subscribers.json"
-
+######################### DATA STRUCTURES #################################
+# Example slice and subscriber data for testing.
 slice_data = {
     "slice_1": {
         "sst": 1,
-        "sd": "000001",
+        "sd": "0xFFFFFF", 
         "default_indicator": True,
         "session": [
             {
@@ -46,8 +55,8 @@ slice_data = {
         ],
     },
     "slice_2": {
-        "sst": 2,
-        "sd": "000002",
+        "sst": 1,
+        "sd": "000001",
         "default_indicator": True,
         "session": [
             {
@@ -70,11 +79,10 @@ slice_data = {
         ],
     },
 }
-
-subscriber_data = {
+simulated_subscriber_data = {
     "subscriber_1": {
         "_id": "",
-        "imsi": "001010000000001",
+        "imsi": "001010000000006",
         "subscribed_rau_tau_timer": 12,
         "network_access_mode": 0,
         "subscriber_status": 0,
@@ -85,17 +93,17 @@ subscriber_data = {
             "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
         },
         "security": {
-            "k": "465B5CE8B199B49FAA5F0A2EE238A6BC",
+            "k": "fec86ba6eb707ed08905757b1bb44b8f",
             "amf": "8000",
             "op": None,
-            "opc": "E8ED289DEBA952E4283B54E88E6183CA",
+            "opc": "C42449363BBAD02B66D16BC975D77CC1",
         },
         "schema_version": 1,
         "__v": 0,
     },
     "subscriber_2": {
         "_id": "",
-        "imsi": "001010000000002",
+        "imsi": "001010000000007",
         "subscribed_rau_tau_timer": 12,
         "network_access_mode": 0,
         "subscriber_status": 0,
@@ -106,17 +114,17 @@ subscriber_data = {
             "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
         },
         "security": {
-            "k": "B199B49F465B5CE8E238A6BCAA5F0A2E",
+            "k": "fec86ba6eb707ed08905757b1bb44b8f",
             "amf": "8000",
             "op": None,
-            "opc": "283B54E8E8ED289D8E6183CAEBA952E4",
+            "opc": "C42449363BBAD02B66D16BC975D77CC1",
         },
         "schema_version": 1,
         "__v": 0,
     },
     "subscriber_3": {
         "_id": "",
-        "imsi": "001010000000003",
+        "imsi": "001010000000008",
         "subscribed_rau_tau_timer": 12,
         "network_access_mode": 0,
         "subscriber_status": 0,
@@ -127,17 +135,142 @@ subscriber_data = {
             "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
         },
         "security": {
-            "k": "465B5CE8B199B49FAA5F0A2EE238A6BD",
+            "k": "fec86ba6eb707ed08905757b1bb44b8f",
             "amf": "8000",
             "op": None,
-            "opc": "E8ED289DEBA952E4283B54E88E6183CB",
+            "opc": "C42449363BBAD02B66D16BC975D77CC1",
+        },
+        "schema_version": 1,
+        "__v": 0,
+    },
+    "subscriber_4": {
+        "_id": "",
+        "imsi": "001010000000009",
+        "subscribed_rau_tau_timer": 12,
+        "network_access_mode": 0,
+        "subscriber_status": 0,
+        "access_restriction_data": 32,
+        "slice": [slice_data["slice_2"]],
+        "ambr": {
+            "uplink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+            "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+        },
+        "security": {
+            "k": "fec86ba6eb707ed08905757b1bb44b8f",
+            "amf": "8000",
+            "op": None,
+            "opc": "C42449363BBAD02B66D16BC975D77CC1",
+        },
+        "schema_version": 1,
+        "__v": 0,
+    },
+    "subscriber_5": {
+        "_id": "",
+        "imsi": "001010000000010",
+        "subscribed_rau_tau_timer": 12,
+        "network_access_mode": 0,
+        "subscriber_status": 0,
+        "access_restriction_data": 32,
+        "slice": [slice_data["slice_1"]],
+        "ambr": {
+            "uplink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+            "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+        },
+        "security": {
+            "k": "fec86ba6eb707ed08905757b1bb44b8f",
+            "amf": "8000",
+            "op": None,
+            "opc": "C42449363BBAD02B66D16BC975D77CC1",
+        },
+        "schema_version": 1,
+        "__v": 0,
+    },
+    "subscriber_6": {
+        "_id": "",
+        "imsi": "001010000000011",
+        "subscribed_rau_tau_timer": 12,
+        "network_access_mode": 0,
+        "subscriber_status": 0,
+        "access_restriction_data": 32,
+        "slice": [slice_data["slice_2"]],
+        "ambr": {
+            "uplink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+            "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+        },
+        "security": {
+            "k": "fec86ba6eb707ed08905757b1bb44b8f",
+            "amf": "8000",
+            "op": None,
+            "opc": "C42449363BBAD02B66D16BC975D77CC1",
+        },
+        "schema_version": 1,
+        "__v": 0,
+    },
+    "subscriber_7": {
+        "_id": "",
+        "imsi": "001010000000012",
+        "subscribed_rau_tau_timer": 12,
+        "network_access_mode": 0,
+        "subscriber_status": 0,
+        "access_restriction_data": 32,
+        "slice": [slice_data["slice_1"]],
+        "ambr": {
+            "uplink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+            "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+        },
+        "security": {
+            "k": "fec86ba6eb707ed08905757b1bb44b8f",
+            "amf": "8000",
+            "op": None,
+            "opc": "C42449363BBAD02B66D16BC975D77CC1",
+        },
+        "schema_version": 1,
+        "__v": 0,
+    },
+    "subscriber_8": {
+        "_id": "",
+        "imsi": "001010000000013",
+        "subscribed_rau_tau_timer": 12,
+        "network_access_mode": 0,
+        "subscriber_status": 0,
+        "access_restriction_data": 32,
+        "slice": [slice_data["slice_2"]],
+        "ambr": {
+            "uplink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+            "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+        },
+        "security": {
+            "k": "fec86ba6eb707ed08905757b1bb44b8f",
+            "amf": "8000",
+            "op": None,
+            "opc": "C42449363BBAD02B66D16BC975D77CC1",
+        },
+        "schema_version": 1,
+        "__v": 0,
+    },
+    "subscriber_9": {
+        "_id": "",
+        "imsi": "001010000000014",
+        "subscribed_rau_tau_timer": 12,
+        "network_access_mode": 0,
+        "subscriber_status": 0,
+        "access_restriction_data": 32,
+        "slice": [slice_data["slice_2"]],
+        "ambr": {
+            "uplink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+            "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+        },
+        "security": {
+            "k": "fec86ba6eb707ed08905757b1bb44b8f",
+            "amf": "8000",
+            "op": None,
+            "opc": "C42449363BBAD02B66D16BC975D77CC1",
         },
         "schema_version": 1,
         "__v": 0,
     },
 }
-
-cots_ue_data = {
+cots_subscriber_data = {
     "pixel_1": {
         "_id": "",
         "imsi": "001010000060592",
@@ -180,12 +313,79 @@ cots_ue_data = {
         "schema_version": 1,
         "__v": 0,
     },
+    "sim_97": {
+        "_id": "",
+        "imsi": "001010000060597",
+        "subscribed_rau_tau_timer": 12,
+        "network_access_mode": 0,
+        "subscriber_status": 0,
+        "access_restriction_data": 32,
+        "slice": [slice_data["slice_1"]],
+        "ambr": {
+            "uplink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+            "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+        },
+        "security": {
+            "k": "E8CD15760B3C91F85E750E66EC6688EC",
+            "amf": "8000",
+            "op": None,
+            "opc": "86BA674F840472143F0B06B070F42234",
+        },
+        "schema_version": 1,
+        "__v": 0,
+    },
+    "sim_99": {
+        "_id": "",
+        "imsi": "001010000060599",
+        "subscribed_rau_tau_timer": 12,
+        "network_access_mode": 0,
+        "subscriber_status": 0,
+        "access_restriction_data": 32,
+        "slice": [slice_data["slice_2"]],
+        "ambr": {
+            "uplink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+            "downlink": {"value": 1, "unit": Open5GS.Unit.Gbps},
+        },
+        "security": {
+            "k": "2F6E0B8F5C329503497A816D813FCB5C",
+            "amf": "8000",
+            "op": None,
+            "opc": "85DCACDF39894DAA5D8A6BD18A44EB7D",
+        },
+        "schema_version": 1,
+        "__v": 0,
+    },
 }
 
+######################### HELPER FUNCTIONS #################################
 
-def generate_slice_data(slice_number, qos_index):
+def convert_defaultdict_to_dict(obj):
+    """Recursively converts defaultdict to dict."""
+    if isinstance(obj, defaultdict):
+        obj = dict(obj)  # Convert the defaultdict to a regular dict
+    if isinstance(obj, dict):
+        for key, value in obj.items():
+            obj[key] = convert_defaultdict_to_dict(value)  # Recursively convert nested defaultdicts
+    return obj
+
+def load_existing_data():
+    """Load existing slice and subscriber data if available."""
+    global slice_data, simulated_subscriber_data, cots_subscriber_data
+    try:
+        with open(DEFAULT_CONFIG["SLICE_FILE_PATH"], "r") as file:
+            slice_data = yaml.load(file)
+    except FileNotFoundError:
+        log.warning(f"No existing slice data found at {DEFAULT_CONFIG['SLICE_FILE_PATH']}")
+
+    try:
+        with open(DEFAULT_CONFIG["SUBSCRIBER_FILE_PATH"], "r") as file:
+            subscribers = yaml.load(file)
+    except FileNotFoundError:
+        log.warning(f"No existing subscriber data found at {DEFAULT_CONFIG['SUBSCRIBER_FILE_PATH']}")
+
+def generate_slice_data(slice_number, qos_index=DEFAULT_CONFIG["DEFAULT_QOS_INDEX"]):
     slice_name = f"slice_{slice_number}"
-    sd = f"{slice_number:06d}"
+    sd = f"{slice_number:06x}"
     session_name = f"dnn{slice_number}"
 
     return {
@@ -199,24 +399,22 @@ def generate_slice_data(slice_number, qos_index):
                     "type": Open5GS.Type.IPv4,
                     "pcc_rule": [],
                     "ambr": {
-                        "uplink": DEFAULT_UPLINK_SPEED,
-                        "downlink": DEFAULT_DOWNLINK_SPEED,
+                        "uplink": DEFAULT_CONFIG["DEFAULT_UPLINK_SPEED"],
+                        "downlink": DEFAULT_CONFIG["DEFAULT_DOWNLINK_SPEED"],
                     },
-                    "qos": {"index": qos_index, "arp": DEFAULT_ARP_VALUES},
+                    "qos": {"index": qos_index, "arp": DEFAULT_CONFIG["DEFAULT_ARP_VALUES"]},
                 }
             ],
         }
     }
 
-
 def create_slices(num_slices):
-    last_slice_number = max(slice_data.keys(), key=lambda x: int(x.split("_")[1]))
+    """Create slices based on the number specified and add to slice_data."""
+    last_slice_number = max(slice_data.keys(), key=lambda x: int(x.split("_")[1]), default="slice_0")
     starting_slice_number = int(last_slice_number.split("_")[1]) + 1
 
     for i in range(starting_slice_number, starting_slice_number + num_slices):
-        qos_index = DEFAULT_QOS_INDEX
-        slice_data.update(generate_slice_data(i, qos_index))
-
+        slice_data.update(generate_slice_data(i))
 
 def generate_subscriber_data(slice_name, subscriber_index):
     slice_info = slice_data[slice_name]
@@ -224,9 +422,7 @@ def generate_subscriber_data(slice_name, subscriber_index):
     subscriber_name = f"subscriber_{slice_number}{subscriber_index:02d}"
 
     padding_length = 15 - len(f"00101{slice_name.split('_')[1]}{subscriber_index:02d}")
-    imsi = (
-        f"00101{'0' * padding_length}{slice_name.split('_')[1]}{subscriber_index:02d}"
-    )
+    imsi = f"00101{'0' * padding_length}{slice_name.split('_')[1]}{subscriber_index:02d}"
 
     return {
         subscriber_name: {
@@ -238,56 +434,97 @@ def generate_subscriber_data(slice_name, subscriber_index):
             "access_restriction_data": 32,
             "slice": [slice_info],
             "ambr": {
-                "uplink": DEFAULT_UPLINK_SPEED,
-                "downlink": DEFAULT_DOWNLINK_SPEED,
+                "uplink": DEFAULT_CONFIG["DEFAULT_UPLINK_SPEED"],
+                "downlink": DEFAULT_CONFIG["DEFAULT_DOWNLINK_SPEED"],
             },
             "security": {
-                "k": DEFAULT_KEY,
+                "k": DEFAULT_CONFIG["DEFAULT_KEY"],
                 "amf": "8000",
                 "op": None,
-                "opc": DEFAULT_OPC,
+                "opc": DEFAULT_CONFIG["DEFAULT_OPC"],
             },
             "schema_version": 1,
             "__v": 0,
         }
     }
 
-
-def create_subscribers(num_subscribers):
+def create_auto_generated_subscribers():
+    """Create subscribers and assign them to slices in a round-robin fashion."""
     subscribers = {}
-    subscribers.update(subscriber_data)
-    subscribers.update(cots_ue_data)
 
-    num_slices = len(slice_data)
-    slice_counter = 0
-    subscriber_assignments = defaultdict(list)
-    for _ in range(1, num_subscribers + 1):
-        slice_index = slice_counter % num_slices + 1
-        slice_name = f"slice_{slice_index}"
-        subscriber_index = len(subscriber_assignments[slice_name]) + 1
-        subscriber_assignments[slice_name].append(subscriber_index)
-        slice_counter += 1
-        subscriber = generate_subscriber_data(slice_name, subscriber_index)
-        subscribers.update(subscriber)
+    num_auto_generated_subscribers = DEFAULT_CONFIG["NUM_AUTO_GENERATED_SUBSCRIBERS"]
+
+    if num_auto_generated_subscribers > 0:
+        log.info(f"Generating {num_auto_generated_subscribers} auto generated subscribers ...")
+
+        num_slices = DEFAULT_CONFIG["NUM_SLICES"]
+        slice_counter = 0
+        subscriber_assignments = defaultdict(list)
+        for i in range(1, num_auto_generated_subscribers + 1):
+            slice_index = slice_counter % num_slices + 1
+            slice_name = f"slice_{slice_index}"
+            subscriber_index = len(subscriber_assignments[slice_name]) + 1
+            subscriber_assignments[slice_name].append(subscriber_index)
+            subscriber = generate_subscriber_data(slice_name, subscriber_index)
+            subscribers.update(subscriber)
+            slice_counter += 1
 
     return subscribers
 
+def create_cots_subscribers(subscribers):
+    num_cots_subscribers = DEFAULT_CONFIG["NUM_COTS_SUBSCRIBERS"]
+    if num_cots_subscribers > 0:
+        log.info(f"Adding {num_cots_subscribers} COTS subscribers ...")
+        from itertools import islice
+        for key, value in islice(cots_subscriber_data.items(), num_cots_subscribers):
+            subscribers[key] = value
+
+def create_simulated_subscribers(subscribers):
+    num_simulated_subscribers = DEFAULT_CONFIG["NUM_SAMPLE_SUBSCRIBERS"]
+    if  num_simulated_subscribers > 0:
+        log.info(f"Adding {num_simulated_subscribers} sample subscribers ...")
+        from itertools import islice
+        for key, value in islice(simulated_subscriber_data.items(), num_simulated_subscribers):
+            subscribers[key] = value
+
+
+
+######################### MAIN SCRIPT ###################################
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Generate Open5GS subscriber information.")
+    parser.add_argument(
+        "--num-cots-subscribers",
+        type=int,
+        default=DEFAULT_CONFIG["NUM_COTS_SUBSCRIBERS"],
+        help="Number of COTS subscribers (default: %(default)s)"
+    )
+    args = parser.parse_args()
+
+    DEFAULT_CONFIG["NUM_COTS_SUBSCRIBERS"] = args.num_cots_subscribers
+
+    log.info("Loading existing data...")
+    load_existing_data()
+
+    log.info(f"Creating {DEFAULT_CONFIG['NUM_SLICES']} slices ...")
+    slices_to_generate = DEFAULT_CONFIG["NUM_SLICES"] - len(slice_data)
+    log.info(f"Generating {slices_to_generate} new slices...")
+    create_slices(slices_to_generate)
+
+    log.info(f"Saving slices to {DEFAULT_CONFIG['SLICE_FILE_PATH']}")
+    with open(DEFAULT_CONFIG["SLICE_FILE_PATH"], "w") as file:
+        yaml.dump(slice_data, file)
+
+    subscribers = create_auto_generated_subscribers()
+    create_cots_subscribers(subscribers)
+    create_simulated_subscribers(subscribers)
+
+    log.info(f"Saving subscribers to {DEFAULT_CONFIG['SUBSCRIBER_FILE_PATH']}")
+    with open(DEFAULT_CONFIG["SUBSCRIBER_FILE_PATH"], "w") as file:
+        yaml.dump(subscribers, file)
+
+    log.info("Slice and subscriber creation complete.")
 
 if __name__ == "__main__":
-    with open(DATA_DIR + "/config.json", "r") as file:
-        config_file = file.read()
-        config = json.loads(config_file)
-
-    num_slices = config["NUM_SLICES"]
-    existing_slices = len(slice_data)
-
-    log.info(f"Creating slices and saving to {SLICE_FILE_PATH}")
-    create_slices(num_slices - existing_slices)
-    with open(SLICE_FILE_PATH, "w") as file:
-        json.dump(slice_data, file, indent=2)
-
-    log.info(f"Creating subscribers and saving to {SUBSCRIBER_FILE_PATH}")
-    num_subscribers = config["NUM_SUBSCRIBERS"]
-    subscribers = create_subscribers(num_subscribers)
-    with open(SUBSCRIBER_FILE_PATH, "w") as file:
-        json.dump(subscribers, file, indent=2)
+    main()
